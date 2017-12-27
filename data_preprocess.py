e75c1a1adf542406cb2a159777c3e0ee0e0379bb
Amol Kapoor <theahura@gmail.com>
2017-12-27 03:55:13 -0500

    Added multithreaded. System possibly works but crashes memory.

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,46 +1,46 @@
 """
-Rescales, cuts radius, and color balances all data in ./data folder.
+Scales, cuts radius, and color balances all data in ./data folder.
 
 See code at
 https://github.com/btgraham/SparseConvNet/blob/kaggle_Diabetic_Retinopathy_competition/Data/kaggleDiabeticRetinopathy/preprocessImages.py
+
+See code at: https://github.com/JeffreyDF/kaggle_diabetic_retinopathy/blob/master/generators.py
 """
 
-import cv2
 import glob
-import numpy
+
+import cv2
+import IPython
+import numpy as np
+from PIL import Image, ImageChops, ImageOps
 
 DO_TEST = False
-
-def scaleRadius(img, scale):
-    x = img[img.shape[0]/2, :, :].sum(1)
-    r = (x > x.mean()/10).sum()/2
-    s = scale*1.0/r
-    return cv2.resize(img, (0, 0), fx=s, fy=s)
-
-scale = 300
+scale = 180
 
 if DO_TEST:
     fp = 'test/*.jpeg'
 else:
     fp = 'train/*.jpeg'
 
+size = 512, 512
 for i, f in enumerate(glob.glob(fp)):
-    try:
-        # Load the image.
-        a = cv2.imread(f)
-        a = scaleRadius(a, scale)
+    # Resize the image.
+    image = Image.open(f)
+    image.thumbnail(size, Image.ANTIALIAS)
+    image_size = image.size
+    thumb = image.crop((0, 0, size[0], size[1]))
+    offset_x = max((size[0] - image_size[0]) / 2, 0)
+    offset_y = max((size[1] - image_size[1]) / 2, 0)
+    thumb = ImageChops.offset(thumb, offset_x, offset_y)
 
-        # Create a mask.
-        b = numpy.zeros(a.shape)
-        cv2.circle(b, (a.shape[1]/2, a.shape[0]/2), int(scale*0.9), (1, 1, 1), -1, 8, 0)
+    # Radius crop and color balance the image.
+    a = np.asarray(thumb)
+    b = np.zeros(a.shape)
+    cv2.circle(b, (a.shape[1]/2, a.shape[0]/2), int(scale*0.9), (1, 1, 1), -1, 8, 0)
+    aa = cv2.addWeighted(a, 4, cv2.GaussianBlur(a, (0, 0), scale/30), -4, 128)*b+128*(1-b)
 
-        # Apply the mask and the local average subtraction.
-        aa = cv2.addWeighted(a, 4, cv2.GaussianBlur(a, (0, 0), scale/30), -4, 128)*b+128*(1-b)
+    # Save.
+    cv2.imwrite(f, aa)
 
-        # Save.
-        cv2.imwrite(f, aa)
-
-        print i
-        print f
-    except:
-        print f
+    print i
+    print f
--- dr.py
+++ dr.py
@@ -1,40 +1,72 @@
+"""
+Batcher, model, training.
+
+- Make batching multithreaded
+"""
+
 import os
 
+import cv2
 import numpy as np
 import pandas
 from PIL import Image
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
+import threading
 
 import IPython
 
 rng = np.random.RandomState(128)
 
 # Globals, hyperparameters.
-PREPROCESS_DATA_PARAMS = 100
+SUMMARIES_EVERY_N = 5
+VALIDATION_EVERY_N = 100
+BATCH_SIZE = 64
 
-NUM_LABELS = 5
 L2_REG = 0.0005
 LEARN_RATE = 0.1
 
+LAST_N_VAL = 10
+NUM_LABELS = 5
 LABELS_FP = './trainLabels.csv'
-DATA_FP = './data'
+DATA_FP = './train'
+LOGDIR = './logs'
+CKPTS = './ckpts'
 
 
+def get_accuracies(session, preds, labels):
+    accs = np.zeros(5)
+    counts = np.bincount(labels)
+    for p, l in zip(preds, labels):
+        if p == l:
+            accs[l] += 1
+    accs /= counts
 
-def get_dataset_params(data):
-    IPython.embed()
+    summaries = []
+    for i, acc in enumerate(accs):
+        summaries.append(tf.summary.scalar('acc/' + str(i), acc))
+
+    return session.run(tf.summary.merge(summaries))
+
+def open_files(f):
+    return cv2.imread(f)
 
 class Batcher(object):
 
-    def __init__(self):
+    def __init__(self, data_fp, label_fp, validation=False):
 
-        # Set index.
+        # Set initial vars.
         self.index = 0
+        self.val_flag = False
+        self.next_batch_flag = False
+        self.next_batch_data = None
+        self.next_batch_arrays = None
+        self.val_arrays = None
 
         # Get initial data.
-        files = [os.path.join(DATA_FP, f) for f in os.listdir(DATA_FP)]
-        self.data = pandas.read_csv(LABELS_FP)
+        files = [os.path.join(data_fp, f) for f in os.listdir(data_fp)]
+
+        self.data = pandas.read_csv(label_fp)
 
         # Add filepath.
         files = sorted(files, key=lambda f: int(f.split('/')[-1].split('_')[0]))
@@ -47,12 +79,49 @@ class Batcher(object):
         weights = [n/(c * counts[level] * 1.0) for level in self.data['level']]
         self.data['weights'] = weights
 
-        # Get data parameters.
-        self.parameters = get_dataset_params(self.data)
+        # Get onehots.
+        onehots = np.eye(NUM_LABELS, dtype=int)
+        def to_onehot(i):
+            return onehots[i]
+
+        self.data['labels'] = self.data['level'].map(to_onehot)
+
+        # Set the validation set to the last fifth of the training data.
+        if validation:
+            val_slice = len(self.data)/5
+            self.val_data = self.data.iloc[-val_slice:]
+            self.data = self.data.iloc[:-val_slice]
+            print 'Getting validation arrays'
+            threading.Thread(target=self._load_val_arrays).start()
+        threading.Thread(target=self._get_batch, args=(BATCH_SIZE,)).start()
+
+    def _load_val_arrays(self):
+        self.val_arrays = np.stack(self.val_data['filename'].map(open_files).values)
+        self.val_flag = True
+
+    def _get_batch(self, size):
+        batch_data = self.data.iloc[self.index:self.index+size]
+        self.index += size
+
+        if self.index > len(self.data):
+            self.index = 0
+
+        batch_arrays = np.stack(batch_data['filename'].map(open_files).values)
+        self.next_batch_data = batch_data
+        self.next_batch_arrays = batch_arrays
+        self.next_batch_flag = True
 
     def get_batch(self, size):
-        batch_data = self.data[self.index:self.index+size]
-        pass
+        while not self.next_batch_flag: pass;
+        next_batch = self.next_batch_data, self.next_batch_arrays
+        self.next_batch_flag = False
+        threading.Thread(target=self._get_batch, args=(size,)).start()
+        return next_batch
+
+    def get_validation_batch(self, size):
+        while not self.val_flag: pass;
+        indices = np.random.choice(len(self.val_data), size, replace=False)
+        return self.val_data.iloc[indices], self.val_arrays[indices]
 
 # Create network.
 class Model(object):
@@ -91,23 +160,82 @@ class Model(object):
             net = slim.fully_connected(net, 1024)
             net = slim.dropout(net, 0.5, is_training=is_training)
             self.logits = slim.fully_connected(net, NUM_LABELS)
+            self.preds = tf.argmax(self.logits)
 
         # Losses.
-        self.labels = tf.placeholder(tf.float32, shape=(None, NUM_LABELS))
-        self.crossent_loss = tf.losses.softmax_cross_entropy(self.logits,
-                                                             self.labels,
+        self.labels = tf.placeholder(tf.float32, name='labels')
+        self.crossent_loss = tf.losses.softmax_cross_entropy(self.labels,
+                                                             self.logits,
                                                              self.weights)
         self.norm_loss = tf.losses.get_regularization_losses()
         self.total_loss = tf.losses.get_total_loss()
 
         # Optimizer.
+        self.global_step = tf.Variable(0, name='global_step', trainable=False)
         op = tf.train.AdamOptimizer(LEARN_RATE)
         net_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
         self.grads = tf.gradients(self.total_loss, net_vars)
         grads_and_vars = list(zip(self.grads, net_vars))
-        self.train = op.apply_gradients(grads_and_vars)
+        self.train = op.apply_gradients(grads_and_vars,
+                                        global_step=self.global_step)
 
-batcher = Batcher()
+        # Summaries.
+        _loss = tf.summary.scalar('model/tot_loss', tf.reduce_mean(self.total_loss))
+        _reg = tf.summary.scalar('model/reg_losses', tf.reduce_mean(self.norm_loss))
+        _grad_norm = tf.summary.scalar('model/grad_norm', tf.global_norm(self.grads))
+        _var_norm = tf.summary.scalar('model/var_norm', tf.global_norm(net_vars))
+        _logits = tf.summary.histogram('model/preds', self.preds)
+
+        self.summary_op = tf.summary.merge([_loss, _reg, _grad_norm, _var_norm,
+                                            _logits])
+
+        self.saver = tf.train.Saver()
+
+
+# Init batcher, model, session.
+batcher = Batcher(DATA_FP, LABELS_FP, True)
 model = Model()
+sess = tf.Session()
+
+# Summary writer.
+writer = tf.summary.FileWriter(LOGDIR, sess.graph, flush_secs=60)
+
+# Training.
+step = 0
+sess.run(tf.global_variables_initializer())
+for i in range(0, 1000):
+    if step % VALIDATION_EVERY_N == 0 and step > 1:
+        print 'Validation'
+        model.saver.save(sess, CKPTS, global_step=sess.run(model.global_step))
+        batch_data, batch_arrays = batcher.get_validation_batch(BATCH_SIZE)
+        inputs = {model.x: batch_arrays}
+        preds = sess.run(model.preds, inputs)
+        accuracy = get_accuracies(sess, preds, batch_data['level'].as_matrix())
+
+        writer.add_summary(tf.Summary.FromString(accuracy),
+                           sess.run(model.global_step))
+
+        writer.flush()
+    else:
+        batch_data, batch_arrays = batcher.get_batch(BATCH_SIZE)
+        print "Got batch"
+        inputs = {
+            model.x: batch_arrays,
+            model.weights: batch_data['weights'].as_matrix(),
+            model.labels: np.stack(batch_data['labels'].values)
+        }
+
+        if step % SUMMARIES_EVERY_N == 0 and step > 1:
+            print 'Summary'
+            fetched = sess.run([model.train, model.summary_op], inputs)
+            writer.add_summary(tf.Summary.FromString(fetched[1]),
+                               sess.run(model.global_step))
+        else:
+            print 'Training'
+            fetched = sess.run([model.train, model.total_loss], inputs)
+            print 'Loss: %f' % fetched[1]
+
+    step += 1
+    print i
 
 IPython.embed()
