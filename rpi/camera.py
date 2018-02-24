dcaf73d02523331db2924fad4ba92ca0479bde76
Jason Patterson <jp3477@columbia.edu>
2018-02-24 17:10:51 -0500

    addded comments to code

--- rpi/camera.py
+++ rpi/camera.py
@@ -16,95 +16,95 @@ import os
 
 
     
-#Define API_URL
-# API_URL = "http://54.164.115.173:80/upload"
+#Define api urls
 SERV_ADDR = "http://drcam.molabs.tech"
 UPLOAD_URL = SERV_ADDR + "/upload"
 REQUEST_IMAGE_URL = SERV_ADDR + "/image"
 
 try:
- #    camera = picamera.PiCamera()
- #    camera.resolution = (512, 512)
- #    camera.start_preview()
- #    stream = io.BytesIO()
+    #Load picamera and begin preview
+    camera = picamera.PiCamera()
+    camera.resolution = (512, 512)
+    camera.start_preview()
+    stream = io.BytesIO()
     
  #    #Wait for shutter press
 
 	# #Pause before capturing
- #    time.sleep(2)
+    time.sleep(2)
+    camera.capture(stream, 'jpeg')
+    camera.stop_preview()
 
- #    camera.capture(stream, 'jpeg')
- #    camera.stop_preview()
- #    #GUI to interact with server
-    image = Image.open(os.path.expanduser("~/Downloads/217_left.jpeg"))
+    # image = Image.open(os.path.expanduser("~/Downloads/217_left.jpeg"))
+
+    #Process image and save in temp file
+    image = Image.open(stream)
     orig_size = image.size
-
     image.thumbnail((512, 512), PIL.Image.ANTIALIAS)
-
     tf = tempfile.NamedTemporaryFile()
- #    image = Image.open(stream)
-    
     image.save(tf, format='jpeg')
 
-    
+    #Display selection GUI
     print "Loading GUI"
-    # IPython.embed()
     reply = ccbox("Do you want to analyze this image?", "Image Viewer", 
         image=tf.name)
-    print "GUI Loaded"
 
+    #Resize image back to original size for server
     image = image.resize(orig_size)
-
-    image_file =  io.BytesIO()
-    image.save(image_file, format='jpeg')
+    image_binary =  io.BytesIO()
+    image.save(image_binary, format='jpeg')
     image_file.seek(0)
 
+    #Send uploaded image to server
     if reply == True:
-        with open(tf.name, "r") as f:   
-            #Send image to server             
-            print "Sending image to server"
-            res = requests.post(
-                UPLOAD_URL,
-                files={'image': ('image.jpg', image_file)}
-            )
+        #Send image to server             
+        print "Sending image to server"
+        res = requests.post(
+            UPLOAD_URL,
+            files={'image': ('image.jpg', image_binary)}
+        )
+        res.raise_for_status()
+        res_json = res.json()
+
+        #Diabetic retinopathy prediction
+        pred = res_json.pop("pred", None)
+
+        #Collect processed images for display
+        print "Obtaining model results..."
+        image_dict = {}
+        for im in res_json:
+            image_url = REQUEST_IMAGE_URL + "/{}".format(res_json[im])
+
+            res = requests.get(image_url)
             res.raise_for_status()
-            #Parse response
-            res_json = res.json()
-            pred = res_json.pop("pred", None)
+            im_data = Image.open(io.BytesIO(res.content))
 
-            image_dict = {}
-            for im in res_json:
-                image_url = REQUEST_IMAGE_URL + "/{}".format(res_json[im])
-
-                res = requests.get(image_url)
-                im_data = Image.open(io.BytesIO(res.content))
-
-                image_dict[im] = im_data
-                time.sleep(0.3)
-
-            fig = plt.figure()
-
-            plt.subplot(221)
-            plt.imshow(image)
-            plt.title('Original')
-
-            plt.subplot(222)
-            plt.imshow(image_dict["im_p"])
-            plt.title("Im P")
-
-            plt.subplot(223)
-            plt.imshow(image_dict["hm"])
-            plt.title("Heatmap")
-
-            plt.subplot(224)
-            plt.imshow(image_dict["hm_im"])
-            plt.title("Headmap Superimposed Image")
+            image_dict[im] = im_data
+            time.sleep(0.1)
 
 
-            plt.show()
+        #Display results to user
+        fig = plt.figure()
+
+        plt.subplot(221)
+        plt.imshow(image)
+        plt.title('Original Image')
+
+        plt.subplot(222)
+        plt.imshow(image_dict["im_p"])
+        plt.title("Processed Image")
+
+        plt.subplot(223)
+        plt.imshow(image_dict["hm"])
+        plt.title("Heatmap")
+
+        plt.subplot(224)
+        plt.imshow(image_dict["hm_im"])
+        plt.title("Heatmap Superimposed Image")
+
+        plt.show()
 
 
-            print("Request sent to server")
 except:
 	raise
 finally:
